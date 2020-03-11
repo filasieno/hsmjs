2c7c080342be31589a4f78261b6757bb232303b6
Fabio Filasieno <dev@db4c.io>
2020-03-11 22:16:29 +0100

    Move to tsc 3.8; fix lint

--- src/taskqueue.js
+++ src/taskqueue.js
@@ -1,17 +1,14 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        let v = factory(require, exports);
+(function(factory) {
+    if (typeof module === 'object' && typeof module.exports === 'object') {
+        const v = factory(require, exports);
         if (v !== undefined) module.exports = v;
     }
-    else if (define && typeof define === "function" && define.amd) {
-        define(["require", "exports", "queue"], factory);
+    else if (define && typeof define === 'function' && define.amd) {
+        define(['require', 'exports', 'queue'], factory);
     }
-})(function (require, exports) {
-
-    const queue = require("queue");
-
+})(function(require, exports) {
+    const queue = require('queue');
     exports.createTaskQueue = function createTaskQueue() {
-        return queue({concurrency:1, timeout: 0, autostart: true});
+        return queue({concurrency: 1, timeout: 0, autostart: true});
     };
-
 });
